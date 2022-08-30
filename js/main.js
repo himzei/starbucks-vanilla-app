5792dd20bdf600ec179ea81a9eb30bacb3e464e6
조현일 <himzei@gmail.com>
2022-08-31 01:42:42 +0900

    gsap

--- js/main.js
+++ js/main.js
@@ -13,3 +13,27 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+// 배지 일정부분에서 없애주기
+const badgeEl = document.querySelector("header .badges");
+// 스크롤 할때마다 함수가 실행 되므로 홈페이지가 무거워질수 있다.
+window.addEventListener(
+  "scroll",
+  // _.throttle(함수, 시간)
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
