75e9afdff48b82a1e5540f2f41fc1781f93512ce
조현일 <himzei@gmail.com>
2022-09-01 08:20:59 +0900

    firebase setting"

---
+++ public/js/firebase.js
@@ -0,0 +1,16 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuhkCYwL8XL4d12RRT1BQNPhAl-evX6rI",
+  authDomain: "ken-s-programming-note.firebaseapp.com",
+  projectId: "ken-s-programming-note",
+  storageBucket: "ken-s-programming-note.appspot.com",
+  messagingSenderId: "123105724625",
+  appId: "1:123105724625:web:7c115e2fc1348a04eb5df0",
+  measurementId: "G-2YDRVHQFW6",
+};
+
+
+ const app = firebase.initializeApp(firebaseConfig);
+
+
